555c7d71bf726a2d82e2fd0d46c18cc47e44b6c8
tubleronchik <m.bystramovich@gmail.com>
2021-12-15 16:49:38 +0300

    add new measurements

--- src/utils/measurement.js
+++ src/utils/measurement.js
@@ -160,6 +160,46 @@ export const measurements = {
     label: "Airtemp",
     unit: "℃",
   },
+  airTempMin: {
+    label: "MinAirtemp",
+    unit: "℃",
+  },
+  airTempAvg: {
+    label: "AvgAirtemp",
+    unit: "℃",
+  },
+  airTempMax: {
+    label: "MaxAirtemp",
+    unit: "℃",
+  },
+  soiltemp: {
+    label: "Soiltemp",
+    unit: "℃",
+  },
+  rainfall: {
+    label: "Rainfall",
+    unit: "mm",
+  },
+  sat5: {
+    label: "Sat5",
+    unit: "",
+  },
+  sat10: {
+    label: "Sat10",
+    unit: "",
+  },
+  windang: {
+    label: "Windang",
+    unit: "°",
+  },
+  windspeed: {
+    label: "Windspeed",
+    unit: "m/s",
+  },
+  windspeedmax: {
+    label: "Windspeedmax",
+    unit: "m/s",
+  },
   pm1: {
     label: "PM1",
     unit: "PM1",
