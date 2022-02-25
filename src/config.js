ae04701ab50f0bad45f0a1e71bda90fc09343359
vol4tim <vol4tim@gmail.com>
2022-02-25 15:39:50 +0400

    disable 1.pubsub.aira.life

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ export default {
       config: {
         Addresses: {
           Swarm: [
-            "/dns4/1.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
+            // "/dns4/1.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
             "/dns4/2.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
             "/dns4/3.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
           ],
         },
         Bootstrap: [
-          "/dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8",
+          // "/dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8",
           "/dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9",
           "/dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw",
         ],
@@ -30,6 +30,7 @@ export default {
     ],
   },
   REMOTE_PROVIDER: "https://roseman.airalab.org/",
+  WIND_PROVIDER: "https://wind.airalab.org/latest",
   MAP: {
     zoom: "13",
     position: {
